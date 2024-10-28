84a8cb8a11c9cd8978c1c68c0a975bd6bfe36e44
Trentyne Morgan <77609868+TrentyneM@users.noreply.github.com>
2024-10-27 22:22:39 -0400

    Add files via upload

--- IdenticalArrays.java
+++ IdenticalArrays.java
@@ -1,18 +1,23 @@
 package com.mycompany.identicalarrays;
 
-import java.util.Scanner;
+import java.util.Scanner;   // Used for Java User Input
 
 public class IdenticalArrays {
 
     public static void main(String[] args) {
         
+        // Creating a new scanner object for user input. 
         Scanner input = new Scanner(System.in);
+        
+        // Declaring and Initalizing 2 new 2D 3 x 3 arrays
+        int[][] m1 = new int[3][3];
+        int[][] m2 = new int[3][3];
 
         // Prompt the user to enter the first array
         System.out.println("Enter the first 3 by 3 array: ");
-        
-        // Declaring and Initalizing a new 2D 3 x 3 array
-        int[][] m1 = new int[3][3];
+
+        // Nested for loops to iterate and fill the first 3 x 3 array with user input
+        // i is used for the first array index, j is used for the second array index. 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 m1[i][j] = input.nextInt();
@@ -21,7 +26,6 @@ public class IdenticalArrays {
 
         // Prompt the user to enter the second array
         System.out.println("Enter the second 3 by 3 array: ");
-        int[][] m2 = new int[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 m2[i][j] = input.nextInt();
@@ -36,13 +40,8 @@ public class IdenticalArrays {
         }
     }
 
-    /**
-     * Returns true if the two-dimensional arrays m1 and m2 are identical.
-     *
-     * @param m1 The first two-dimensional array.
-     * @param m2 The second two-dimensional array.
-     * @return true if m1 and m2 are identical, false otherwise.
-     */
+
+    /* This method checks if array size and contents are same*/
     public static boolean equals(int[][] m1, int[][] m2) {
         // Check if the two arrays have the same dimensions
         if (m1.length != m2.length || m1[0].length != m2[0].length) {
